a88c1e1fe240bb84bab904aa4da80d362baf8e97
Onurege <78586675+onure9e@users.noreply.github.com>
2022-12-04 07:45:49 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
+using System;
 using System.IO;
 
-namespace diziler
+namespace method_definition
 {
     class Program
     {
@@ -9,41 +9,20 @@ namespace diziler
        {
            int a=2;
            int b=3;
-           int sonuc =Topla(a,b); 
-           Console.WriteLine(sonuc);
+           int sonuc =(a+b);
+           Console.WriteLine(Convert.ToString(sonuc));
 
-           Metotlar ornek= new Metotlar();
-           ornek.EkranaYazdir(Convert.ToString(sonuc));
-
-           int sonuc2=ornek.ArttirveTopla(ref a,ref b);
-           ornek.EkranaYazdir(Convert.ToString(sonuc2));
-           ornek.EkranaYazdir(Convert.ToString(a+b));
+           int sonuc2=a+1+b+1;
+           Console.WriteLine(Convert.ToString(sonuc2));
+           Console.WriteLine(Convert.ToString(a+b));
                            
        }
 
-       static int Topla(int deger1, int deger2)
-       {
-           return(deger1+deger2);
-       }
 
     }
 
-    class Metotlar
-    {
-        public void EkranaYazdir(string veri)
-        {
-            Console.WriteLine(veri);
-        }
-
-        public int ArttirveTopla(ref int deger1, ref int deger2)
-        {
-        deger1+=1;
-        deger2+=1;
-        return deger1+deger2;
-        }
 
     }
 
     
 
-}
